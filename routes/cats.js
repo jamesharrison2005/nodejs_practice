447a3b20c74e12201279dce52d7374fc62eb164f
semajh <jamesharrison2262@gmail.com>
2026-06-02 13:49:42 +0100

    very basic get and post request with different endpoints using body parameters

--- routes/cats.js
+++ routes/cats.js
@@ -2,11 +2,34 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+
+    
+    console.log('cats headers:', req.headers);
+
+    const apiKey = req.get('apikey');
+
+    if (apiKey !== 'secret') {
+        return res.status(403).send({message: 'Invalid API Key'})
+    }
+
     res.status(200).send({
         name: "Sweep",
         breed: "Manx Cat",
         weight: "11kg"
     })
 })
+
+router.post('/', (req, res) => {
+    const { name, breed } = req.body || {};
+
+    if (!name) return res.status(418).send({ message: "Cat must have a name" });
+    if (!breed) return res.status(418).send({ message: "Cat must have a breed" });
+
+    return res.status(201).send({
+        name: name,
+        breed: breed,
+        weight: "11kg"
+    });
+});
 
 module.exports = router;
--- routes/dogs.js
+++ routes/dogs.js
@@ -2,11 +2,33 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+
+    console.log('dogs headers:', req.headers);
+
+    const apiKey = req.get('apikey')
+
+    if(apiKey !== 'secret'){
+        return res.status(403).send({message: "Invalid API Key"})
+    }
+
     res.status(200).send({
         name: "Izzy",
         breed: "Hungarian Vizsla",
         weight: "25kg"
     })
 })
+
+router.post('/', (req, res) => {
+    const { name, breed } = req.body || {};
+
+    if (!name) return res.status(418).send({ message: "Dog must have a name" });
+    if (!breed) return res.status(418).send({ message: "Dog must have a breed" });
+
+    return res.status(201).send({
+        name: name,
+        breed: breed,
+        weight: "11kg"
+    });
+});
 
 module.exports = router;
